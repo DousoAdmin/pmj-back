9eaba0e00786b270c21faab34be7da919b840b8c
Santiago Salamanca <sansalrus@gmail.com>
2026-05-07 22:31:58 -0500

    persons y personchemas: en beneficiarys y ethnicity Models se modifica el tipo de dato BINARY por BOOLEAN, y en ethnicity_schema se modifico el tipo de dato byte por bool

--- Schemas/personsSchemas/ethnicity_schema.py
+++ Schemas/personsSchemas/ethnicity_schema.py
@@ -4,18 +4,18 @@ from typing import Optional
 class EthnicityCreate(BaseModel):
     ETNC_name: str = Field(..., min_length=1, max_length=45)
     ETNC_description: Optional[str] = Field(None, max_length=155)
-    ETNC_state: Optional[bytes] = None
+    ETNC_state: Optional[bool] = None
 
 class EthnicityUpdate(BaseModel):
     ETNC_name: Optional[str] = Field(None, min_length=1, max_length=45)
     ETNC_description: Optional[str] = Field(None, max_length=155)
-    ETNC_state: Optional[bytes] = None
+    ETNC_state: Optional[bool] = None
 
 class EthnicityResponse(BaseModel):
     ETNC_PK: int
     ETNC_name: str
     ETNC_description: Optional[str]
-    ETNC_state: Optional[bytes]
+    ETNC_state: Optional[bool]
     
     class Config:
         from_attributes = True
